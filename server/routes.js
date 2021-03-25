e3ce2ca8f496bdbbe7e5af3d4719939145b14022
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2021-03-25 17:35:08 -0300

    Preparando rotas de para manipulação de UserProfile

--- server/routes.js
+++ server/routes.js
@@ -1,15 +1,32 @@
 const express = require('express');
-const {pacienteFindAll} = require("./Paciente");
+const {pacienteFindAll} = require("./UserProfile");
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-    pacienteFindAll()
-    .then(paciente => {
-        return paciente
-    })
-    .then(response => {
-        return res.json(response)
-    })
+    // pacienteFindAll()
+    // .then(paciente => {
+    //     return paciente
+    // })
+    // .then(response => {
+    //     return res.json(response)
+    // })
 })
+
+
+routes.route('/userprofile/:id')
+.get((req, res) => {
+    res.json({"id":req.params.id})
+})
+.put((req, res) => {
+    res.json({"id":req.params.id})
+})
+.delete((req, res) => {
+    res.json({"id":req.params.id})
+})
+
+routes.post('/userprofile', (req, res) => {
+    res.json(req.body)
+})
+
 
 module.exports = routes;
