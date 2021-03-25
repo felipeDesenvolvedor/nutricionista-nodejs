d6c9919783726960010aa8d44ded8dab21ad5264
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2021-03-25 17:39:38 -0300

    Delete db.js

--- server/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize, DataTypes, Model } = require('sequelize');
-
-const sequelize = new Sequelize('nutricionista', 'root', '80178RooT@!*', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = {sequelize, DataTypes, Model}
